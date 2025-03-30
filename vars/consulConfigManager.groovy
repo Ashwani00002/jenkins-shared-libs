58ccb1c50515ff8c1b253e1ee5b027c1a49e2dc1
Ashwani00002 <ashwaniverma00002@gmail.com>
2025-03-30 22:10:14 +0530

    abcd

--- vars/consulConfigManager.groovy
+++ vars/consulConfigManager.groovy
@@ -1,11 +1,14 @@
 def call(Map config = [:]) {
-    def consulVersion = config.consulVersion ?: '1.10.0'
-    def consulAddr = config.consulAddr ?: 'http://34.238.184.38:8500/v1/kv'
+    def consulAddr = env.CONSUL_HTTP_ADDR  // Use environment variable directly
+
+    if (!consulAddr) {
+        error "CONSUL_HTTP_ADDR is not defined in the pipeline environment."
+    }
 
     try {
         // Install Consul
         sh """
-            curl -sSL https://releases.hashicorp.com/consul/${consulVersion}/consul_${consulVersion}_linux_amd64.zip -o consul.zip
+            curl -sSL https://releases.hashicorp.com/consul/${config.consulVersion ?: '1.10.0'}/consul_${config.consulVersion ?: '1.10.0'}_linux_amd64.zip -o consul.zip
         """
         unzip zipFile: 'consul.zip'
         sh "chmod +x consul && rm -f consul.zip"
@@ -20,7 +23,6 @@ def call(Map config = [:]) {
         if (existingKeysResponse) {
             def parsedResponse = readJSON text: existingKeysResponse
             parsedResponse.each { item ->
-                // Extract just the key name (last part of the path)
                 def keyName = item.Key.tokenize('/').last()
                 existingKeys[keyName] = [
                     value: item.Value,
@@ -47,43 +49,37 @@ def call(Map config = [:]) {
         jsonFile.each { key, value ->
             def fullPath = "${env.ENV}/${env.CLUSTER}/${env.APPLICATION_CONFIG_MAP}/${key}"
             
-            // Check if this is a renamed key (value matches an existing key's value)
             def oldKey = existingKeys.find { it.value.value == value }?.key
             
             if (oldKey && oldKey != key) {
-                // This is a renamed key - delete the old one
                 def oldFullPath = existingKeys[oldKey].fullPath
                 sh """
                     curl -k -X DELETE '${consulAddr}/${oldFullPath}'
                 """
                 changes.deleted << oldKey
-                existingKeys.remove(oldKey)  // Remove from tracking
+                existingKeys.remove(oldKey)
                 echo "Deleted renamed key: ${oldKey} (path: ${oldFullPath})"
             }
             
-            // Check if we need to update/create the key
             if (!existingKeys.containsKey(key)) {
-                // New key
                 sh """
                     curl -k -X PUT -d '${value}' '${consulAddr}/${fullPath}'
                 """
                 changes.created << key
-                existingKeys[key] = [value: value, fullPath: fullPath]  // Add to tracking
+                existingKeys[key] = [value: value, fullPath: fullPath]
                 echo "Created new key: ${key} (path: ${fullPath})"
             } else if (existingKeys[key].value != value) {
-                // Updated key
                 sh """
                     curl -k -X PUT -d '${value}' '${consulAddr}/${fullPath}'
                 """
                 changes.updated << key
-                existingKeys[key] = [value: value, fullPath: fullPath]  // Update tracking
+                existingKeys[key] = [value: value, fullPath: fullPath]
                 echo "Updated key: ${key} (path: ${fullPath})"
             } else {
                 echo "Skipped: ${key} (already exists with correct value)"
             }
         }
 
-        // Identify keys to delete (present in Consul but not in config file)
         def keysToDelete = existingKeys.keySet().findAll { !jsonFile.containsKey(it) }
         keysToDelete.each { key ->
             def fullPath = existingKeys[key].fullPath
@@ -94,7 +90,6 @@ def call(Map config = [:]) {
             echo "Deleted removed key: ${key} (path: ${fullPath})"
         }
 
-        // Print summary of all changes
         echo "\n=== CONSUL CONFIGURATION CHANGES SUMMARY ==="
         if (changes.created) {
             echo "Created keys (${changes.created.size()}):"
